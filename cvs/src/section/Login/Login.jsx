d09b59139dc295c22d5615b8f807bedec2dab539
VIKAS <vikasraw17@gmail.com>
2024-07-27 22:28:46 +0530

    commit

---
+++ cvs/src/section/Login/Login.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function Login() {
+  return (
+    <>
+
+    <section id='login-form'>
+
+        <form className='login'>
+
+            
+
+
+
+        </form>
+    </section>
+    
+    </ >
+  )
+}
+
+export default Login
