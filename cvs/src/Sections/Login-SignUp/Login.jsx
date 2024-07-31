be62c76cbec997b73aa1a53b2d5d48fa4c22155b
VIKAS <vikasraw17@gmail.com>
2024-07-31 12:38:04 +0530

    commit

--- cvs/src/Sections/Login-SignUp/Login.jsx
+++ cvs/src/Sections/Login-SignUp/Login.jsx
@@ -2,10 +2,43 @@ import React from 'react';
 
 function Login() {
   return (
+
     <div className='Login'>
-        
-    </div>
-  )
-}
+        <div className='header'>
+          <div className='text'>Sign Up</div>
+          <div className='underline'></div>
+        </div>
+
+        <div className='inputs'>
+
+          <div className='input'>
+            <img src='' alt='' />
+            <input type='text' />
+          </div>
+
+          <div className='input'>
+            <img src='' alt='' />
+            <input type='email' />
+          </div>
+
+          <div className='input'>
+            <img src='' alt='' />
+            <input type='password' />
+          </div>
+
+          </div>
+
+          <div className='forgot-pass'>Forget Password </div>
+          <div className='sumbit-container'>
+          <div className='submit'> Sign Up</div>
+          <div className='submit'> Login </div>
+          </div>
+
+
+
+        </div>
+
+  );
+};
 
 export default Login
